f971375f25d60470663292bd34dc8b6fe9a322b6
Emre <eemmrree0@outlook.com>
2021-10-25 18:26:19 +0300

    DataAccess'te ICategoryDal düzenlendi

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-
+    
     }
 }
