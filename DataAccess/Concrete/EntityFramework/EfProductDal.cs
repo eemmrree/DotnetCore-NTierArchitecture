f4ca428a9c64de192521619183256019267ad611
Emre <eemmrree0@outlook.com>
2021-10-26 17:25:19 +0300

    DataAccess EfProductDal'da Listeleme,Ekleme,Silme,Güncelleme metodları yazıldı.

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,39 +1,54 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccess.Abstract;
-using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal:IProductDal
     {
+
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+           
+            using var context = new NorthwindContext();
+            return filter == null 
+                ? context.Set<Product>().ToList() 
+                : context.Set<Product>().Where(filter).ToList();
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            using var context = new NorthwindContext();
+            return context.Set<Product>().SingleOrDefault(filter);
         }
 
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            //IDisposable pattern implementation of c#
+            using var context = new NorthwindContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
         }
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            using var context = new NorthwindContext();
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            using var context = new NorthwindContext();
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
         }
     }
 }
