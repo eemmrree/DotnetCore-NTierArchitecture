91a3256cba7c60c01b498522d84a01afd207a890
Emre <eemmrree0@outlook.com>
2021-10-28 00:29:32 +0300

    EfProductDal düzenlendi.

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -4,51 +4,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Core.DataAccess.EntityFramework;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal:IProductDal
+    public class EfProductDal:EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-           
-            using var context = new NorthwindContext();
-            return filter == null 
-                ? context.Set<Product>().ToList() 
-                : context.Set<Product>().Where(filter).ToList();
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
+        public List<ProductDetailDto> GetProductDetails()
         {
             using var context = new NorthwindContext();
-            return context.Set<Product>().SingleOrDefault(filter);
-        }
-
-        public void Add(Product entity)
-        {
-            //IDisposable pattern implementation of c#
-            using var context = new NorthwindContext();
-            var addedEntity = context.Entry(entity);
-            addedEntity.State = EntityState.Added;
-            context.SaveChanges();
-        }
-
-        public void Update(Product entity)
-        {
-            using var context = new NorthwindContext();
-            var updatedEntity = context.Entry(entity);
-            updatedEntity.State = EntityState.Modified;
-            context.SaveChanges();
-        }
-
-        public void Delete(Product entity)
-        {
-            using var context = new NorthwindContext();
-            var deletedEntity = context.Entry(entity);
-            deletedEntity.State = EntityState.Deleted;
-            context.SaveChanges();
+            var result = from p in context.Products
+                join c in context.Categories
+                    on p.CategoryId equals c.CategoryId
+                select new ProductDetailDto
+                {
+                    ProductId = p.ProductId, ProductName = p.ProductName,
+                    CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock
+                };
+            return result.ToList();
         }
     }
 }
