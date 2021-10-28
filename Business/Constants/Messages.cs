57385ae48aacbde2ca1bd395aeeee1ca85955c0a
Emre <eemmrree0@outlook.com>
2021-10-28 16:14:21 +0300

    Messages sınıfı eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        internal static string ProductAdded = "Ürün Eklendi.";
+        internal static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static string MaintenanceTime = "Zaman Aşımı.";
+        internal static string ProductsListed = "Ürünler Listelendi.";
+        internal static string ProductDetailsListed = "Ürün Detayları Listelendi.";
+        internal static string CategoryById = "Ürünler Kategoriye göre Listelendi.";
+
+    }
+}
