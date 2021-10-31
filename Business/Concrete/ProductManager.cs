09e7ed9b07ef4128374ebd4a95f321d0782e96ad
Emre <eemmrree0@outlook.com>
2021-10-31 22:51:06 +0300

    Business'ta ProductManager yeniden düzenlendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 12)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
